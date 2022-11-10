a40cb3e6441967d3b6b574ffdf3592ef45c51e96
Roqia Ahmed <roqiaahmed447@gmail.com>
2022-11-10 18:21:34 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="main">
     <div class="container">
-      <div class="info-box">
+      <div
+        class="info-box"
+        v-if="state.username === '' || state.username === null"
+      >
         <form action="">
           <h1>log in to chat</h1>
           <label for="Username">Username</label>
@@ -10,25 +13,40 @@
             name=""
             id="Username"
             placeholder="please enter your Username"
+            v-model="inputUsername"
           />
-          <button type="button">Log in</button>
+          <button type="button" @click="signin">Log in</button>
         </form>
       </div>
-      <div class="view-chat">
-        <span>logout</span>
-        <h1>Hello, name</h1>
+
+      <div class="view-chat" v-else>
+        <span @click="logout">logout</span>
+        <h1>Hello, {{ state.username }}</h1>
         <div class="chat-box">
-          <div class="message-box">
-            <span>name</span>
+          <div
+            v-for="message in state.messages"
+            :key="message.key"
+            :class="
+              message.username == state.username
+                ? 'message-box'
+                : 'message-box-anther'
+            "
+          >
+            <span>{{ message.username }}</span>
             <div class="message">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              {{ message.content }}
             </div>
           </div>
         </div>
         <footer>
-          <form action="">
-            <input type="text" class="text" placeholder="Enter your message" />
-            <input type="button" class="button" value="send" />
+          <form action="" @submit.prevent="SendMessage">
+            <input
+              type="text"
+              class="text"
+              placeholder="Enter your message"
+              v-model="inputmessage"
+            />
+            <input type="submit" class="button" value="send" />
           </form>
         </footer>
       </div>
@@ -37,12 +55,76 @@
 </template>
 
 <script>
-// import db from "./db";
+import db from "./db.js";
+import { ref as fbref, push, onValue } from "firebase/database";
+
+import { reactive, ref, onMounted } from "vue";
+export default {
+  setup() {
+    const inputmessage = ref("");
+    const inputUsername = ref("");
+    const state = reactive({
+      username: "",
+      messages: [],
+    });
+    const signin = () => {
+      if (inputUsername.value !== "" || inputUsername.value !== null) {
+        state.username = inputUsername.value;
+        inputUsername.value = "";
+      }
+    };
+    const logout = () => {
+      state.username = "";
+    };
+
+    const SendMessage = () => {
+      const messagesRef = fbref(db, "messages");
+
+      if (inputmessage.value === "" || inputmessage.value === null) {
+        return;
+      }
+      const message = {
+        username: state.username,
+        content: inputmessage.value,
+      };
+      console.log(inputmessage.value);
+
+      push(messagesRef, message);
+
+      inputmessage.value = "";
+    };
+    onMounted(() => {
+      const messagesRef = fbref(db, "messages");
+      onValue(messagesRef, (snapshot) => {
+        const data = snapshot.val();
+        let messages = [];
+        Object.keys(data).forEach((key) => {
+          messages.push({
+            id: key,
+            username: data[key].username,
+            content: data[key].content,
+          });
+        });
+        state.messages = messages;
+        console.log(messages);
+      });
+    });
+
+    return {
+      signin,
+      logout,
+      inputmessage,
+      state,
+      inputUsername,
+      SendMessage,
+    };
+  },
+};
 </script>
 <style lang="scss">
 .main {
   width: 100%;
-  height: 300vh;
+  height: 100vh;
   background-color: rgba(0, 244, 27, 0.2705882353);
   margin: 0;
   padding-top: 15%;
@@ -89,9 +171,11 @@
   }
   .view-chat {
     color: #fff;
+    margin-top: -155px;
 
     span {
       float: right;
+      cursor: pointer;
     }
     .chat-box {
       background-color: #fff;
@@ -101,6 +185,7 @@
       .message-box {
         float: right;
         padding: 30px;
+        display: block;
 
         span {
           color: #dfdcdc;
@@ -111,6 +196,27 @@
         .message {
           color: #fff;
           background-color: #bafcc1;
+          border-radius: 30px;
+          padding: 12px;
+          margin-top: 24px;
+          display: block;
+          margin-bottom: 15px;
+        }
+      }
+      .message-box-anther {
+        float: left;
+        padding: 30px;
+        justify-content: flex-end;
+
+        span {
+          color: #dfdcdc;
+          float: left;
+          margin-left: 20px;
+        }
+
+        .message {
+          color: #737171;
+          background-color: #eef2ee;
           border-radius: 30px;
           padding: 12px;
           margin-top: 24px;
--- src/db.js
+++ src/db.js
@@ -1,14 +1,20 @@
-import firebase from "firebase/app";
-import "firebase/database";
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA83HX3C52jvBIULlStfDizbBthUz-PXCQ",
-    authDomain: "chatapp-a9325.firebaseapp.com",
-    projectId: "chatapp-a9325",
-    storageBucket: "chatapp-a9325.appspot.com",
-    messagingSenderId: "507153053280",
-    appId: "1:507153053280:web:391db30d957aae5e277505",
+  apiKey: "AIzaSyA83HX3C52jvBIULlStfDizbBthUz-PXCQ",
+  authDomain: "chatapp-a9325.firebaseapp.com",
+  projectId: "chatapp-a9325",
+  storageBucket: "chatapp-a9325.appspot.com",
+  messagingSenderId: "507153053280",
+  appId: "1:507153053280:web:391db30d957aae5e277505",
 };
 
-const db = firebase.initializeApp(firebaseConfig);
+// const db = firebase.initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const db = getDatabase(app);
+
 export default db;
